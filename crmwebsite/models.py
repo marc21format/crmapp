a83e993eb4ac989d564729d1bca9e3f2903ec72c
marc21format <mcyoung@up.edu.ph>
2024-11-28 13:38:53 +0800

    added regis updated navbar added database model

--- crmwebsite/models.py
+++ crmwebsite/models.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+class Instructor(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length=50)
+	middle_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	batch = models.CharField(max_length=50)
+	undergrad_course = models.CharField(max_length=100)
+	undergrad_school = models.CharField(max_length=100)
+	undergrad_award = models.CharField(max_length=100)
+	undergrad_scholarship = models.CharField(max_length=100)
+	postgrad_course = models.CharField(max_length=100)
+	postgrad_school = models.CharField(max_length=100)
+	postgrad_award = models.CharField(max_length=100)
+	postgrad_scholarship = models.CharField(max_length=100)
+	other_achievments = models.CharField(max_length=100)
+	status = models.CharField(max_length=10)
+
+	def __str__(self):
+		return(f"{self.first_name} {self.last_name}")
